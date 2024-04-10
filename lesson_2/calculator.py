efee25c4feda6cbcc3a457bd63b73c93ab639551
Justas Zieminykas <justaxsz@gmail.com>
2024-04-10 10:03:26 +0300

    added calculator feature to allow user to do another operation

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,4 +1,14 @@
-import pdb
+"""
+SET keep_going = True
+WHILE keep_going:
+	# do the usual steps: get numbers, operator, display
+	GET again
+	IF again == False:
+		SET keep_going = False
+STOP
+"""
+
+# import pdb
 
 def prompt(message):
     print(f"==> {message}")
@@ -17,48 +27,52 @@ def invalid_number(number_str):
 
 prompt('Welcome to the calculator!')
 
-# Ask the user for the 1st number
-prompt("What's the first number?")
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number. Try again.")
+while True:
+    # Ask the user for the 1st number
+    prompt("What's the first number?")
     number1 = input()
 
-# Ask the user for the 2nd number
-prompt("What's the second number?")
-number2 = input()
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number. Try again.")
+        number1 = input()
 
-# pdb.set_trace()
-
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number. Try again.")
+    # Ask the user for the 2nd number
+    prompt("What's the second number?")
     number2 = input()
 
-# print(f'Number 1: {number1}\nNumber 2: {number2}')
+    # pdb.set_trace()
 
-prompt("""What operation should we do?
-1) Add 2) Subtract 3) Multiply 4) Divide""")
-operation = input()
+    while invalid_number(number2):
+        prompt("Hmm... that doesn't look like a valid number. Try again.")
+        number2 = input()
 
-while operation not in ['1', '2', '3', '4']:
-    prompt("You must enter 1, 2, 3 or 4")
+    # print(f'Number 1: {number1}\nNumber 2: {number2}')
+
+    prompt("""What operation should we do?
+    1) Add 2) Subtract 3) Multiply 4) Divide""")
     operation = input()
 
-number1 = int(number1)
-number2 = int(number2)
+    while operation not in ['1', '2', '3', '4']:
+        prompt("You must enter 1, 2, 3 or 4")
+        operation = input()
 
-match operation:
-    case '1':
-        result = number1 + number2
-    case '2':
-        result = number1 - number2
-    case '3':
-        result = number1 * number2
-    case '4':
-        result = number1 / number2
+    number1 = int(number1)
+    number2 = int(number2)
 
-if result:
-    print(f'The result is: {result}')
-else:
-    print('Wrong selection')
+    match operation:
+        case '1':
+            result = number1 + number2
+        case '2':
+            result = number1 - number2
+        case '3':
+            result = number1 * number2
+        case '4':
+            result = number1 / number2
+
+    prompt(f'The result is: {result}')
+
+    prompt('Should we do another calculation? enter Yes to start again.')
+    again_reply = input()
+
+    if again_reply.lower() != 'yes':
+        break
