c9c3d74f5f05d908febd681e675fca2dd55294de
Justas Zieminykas <justaxsz@gmail.com>
2024-04-11 08:47:37 +0300

    Made improvements to mortgage calculator based on provided solution

--- lesson_2/mortgage.py
+++ lesson_2/mortgage.py
@@ -14,46 +14,50 @@ def is_valid_apr(apr_str):
 
 def is_valid_duration(duration_str):
     try:
-        # Entered duration must be a positive integer. Float not accepted.
-        duration_int = int(duration_str)
-        return (duration_int > 0 and float(duration_str) == duration_int)
+        # Entered duration must be positive
+        return float(duration_str) > 0
     except ValueError:
         return False
 
 # We can define minimum loan amount here
 MINIMUM_AMOUNT = 100
 
-# Getting inputs and making sure they are valid
-loan_amount = input('Enter the loan amount: $')
-while not is_valid_amount(loan_amount):
-    print(f'Invalid amount. Must be higher than {MINIMUM_AMOUNT}. Try again.')
-    loan_amount = input('$')
-loan_amount = float(loan_amount)
+while True:
+    # Getting inputs and making sure they are valid
+    loan_amount = input('Enter the loan amount: $')
+    while not is_valid_amount(loan_amount):
+        print(f'Invalid amount. Must be higher than {MINIMUM_AMOUNT}. Try again.')
+        loan_amount = input('$')
+    loan_amount = float(loan_amount)
 
-apr = input('Enter the annual interest rate percent value (e.g. 2.50): ')
-while not is_valid_apr(apr):
-    print('Invalid APR. It must be either 0 or a positive number. Try again')
-    apr = input()
-apr = float(apr) / 100
+    apr = input('Enter the annual interest rate percent value (e.g. 2.50): ')
+    while not is_valid_apr(apr):
+        print('Invalid APR. It must be either 0 or a positive number. Try again')
+        apr = input()
+    apr = float(apr) / 100
 
-loan_duration_years = input('Enter the loan duration in years: ')
-while not is_valid_duration(loan_duration_years):
-    print('Invalid duration. Must be a positive whole number. Try again.')
-    loan_duration_years = input()
-loan_duration_years = int(loan_duration_years)
+    loan_duration_years = input('Enter the loan duration in years: ')
+    while not is_valid_duration(loan_duration_years):
+        print('Invalid duration. Must be a positive whole number. Try again.')
+        loan_duration_years = input()
+    loan_duration_years = float(loan_duration_years)
 
-# Calculating monthly interest rate if it's non-0
-monthly_rate = apr / 12 if apr > 0 else 0
+    # Calculating monthly interest rate if it's non-0
+    monthly_rate = apr / 12 if apr > 0 else 0
 
-# Calculating loan duration in months
-loan_duration_months = loan_duration_years * 12
+    # Calculating loan duration in months - we round up to the next integer
+    loan_duration_months = round(loan_duration_years * 12)
 
-# Calculating and printing out the monthly payment
-if monthly_rate:
-    monthly_payment = (
-        loan_amount *
-        (monthly_rate / (1 - (1 + monthly_rate) ** (-loan_duration_months)))
-    )
-else:
-    monthly_payment = loan_amount / 12
-print(f'Monthly payment is: ${monthly_payment:.2f}')
+    # Calculating and printing out the monthly payment
+    if monthly_rate:
+        monthly_payment = (
+            loan_amount *
+            (monthly_rate / (1 - (1 + monthly_rate) ** (-loan_duration_months)))
+        )
+    else:
+        monthly_payment = loan_amount / 12
+    print(f'Monthly payment is: ${monthly_payment:.2f}')
+
+    again_answer = input('Another calculation? Enter y or n')
+    if again_answer[0].lower() != 'y':
+        break
