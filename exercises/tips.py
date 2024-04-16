ecfc048ef89c8d2e7484cbe9ebe93eefe6136b8e
Justas Zieminykas <justaxsz@gmail.com>
2024-04-16 09:26:12 +0300

    moved easy_1 exercise solutions to easy_1 subdirectory

--- exercises/tips.py
+++
@@ -1,9 +0,0 @@
-# Asuming user inputs valid numbers
-bill = float(input('What is the bill? '))
-tip_rate = float(input('What is the tip percentage? '))
-
-tip = bill * (tip_rate / 100)
-total_bill = bill + tip
-
-print(f'The tip is ${tip:.2f}')
-print(f'The total is ${total_bill:.2f}')
